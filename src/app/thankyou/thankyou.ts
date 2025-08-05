8732a4a98b69542ea08ff4aed76f0f154f7dbc59
apichat <apichatlasuk13@gmail.com>
2025-08-05 16:26:29 +0700

    bee commit-thankyou

--- src/app/thankyou/thankyou.ts
+++ src/app/thankyou/thankyou.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-thankyou',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './thankyou.html',
-  styleUrl: './thankyou.css'
+  styleUrl: './thankyou.css',
 })
 export class Thankyou {
+  constructor(private router: Router, private location: Location) {
+    this.location.replaceState('');
+  }
 
+  Direct(path: string) {
+    this.router.navigate([path]);
+  }
 }
